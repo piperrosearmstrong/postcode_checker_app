96316782ab09007b6950cd087edf5b5b93068ed8
Piper-Rose Armstrong <piperrosearmstrong@gmail.com>
2022-10-06 18:06:38 +0100

    Debugging Web Applications

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -16,7 +16,7 @@ describe Application do
 
       expect(response.status).to eq(200)
       expect(response.body).to include('<form action="/check" method="POST">')
-      expect(response.body).to include('<input type="text" name="postcode" />')
+      expect(response.body).to include('<input type="text" name="post_code" />')
     end
   end
 
